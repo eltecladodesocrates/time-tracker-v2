a22710659e134d395711931db6acb9ce69696f71
Athos Conde de la Fere <eltecladodesocrates@gmail.com>
2021-02-24 06:07:40 -0500

    remove and track routes

--- src/routes/task.js
+++ src/routes/task.js
@@ -8,7 +8,6 @@ router.use(bodyParser.urlencoded({ extended: true }))
 router.get('/', async (req, res) => {
 
     const tasks = await Task.find()
-    console.log(tasks);
     
     res.render('tasks', {
         tasks
@@ -21,6 +20,18 @@ router.post('/', async (req, res) => {
         name: req.body.newTask
     })
     await task.save()
+    res.redirect('/')
+})
+
+router.post('/delete', async (req, res) => {
+    const task = await Task.findByIdAndDelete(req.body.deleteCheckbox)
+    res.redirect('/')
+})
+
+router.post('/track', async (req, res) => {
+    const task = await Task.findByIdAndUpdate(req.body.trackButton, {
+        time: 25
+    })
     res.redirect('/')
 })
 
