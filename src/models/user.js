ab11dfdcba5695c3d44c51486cf2ed58ad39eb27
Athos Conde de la Fere <eltecladodesocrates@gmail.com>
2021-02-28 00:34:54 -0500

    user model validated

--- src/models/user.js
+++ src/models/user.js
@@ -1,14 +1,34 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 const userSchema = new mongoose.Schema({
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     password: {
-        type: String
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7,
+        validate(value) {
+            if(value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain the string password')
+            }
+        }
     },
     email: {
-        type: String
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if(!validator.isEmail(value)) {
+                throw new Error('Invalid email')
+            }
+        }
     }
 })
 
