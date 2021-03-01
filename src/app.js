7d5093366544b0543a8b5dc82982b58acfe8a6f0
Athos Conde de la Fere <eltecladodesocrates@gmail.com>
2021-03-01 06:46:11 -0500

    hash passwords

--- src/app.js
+++ src/app.js
@@ -17,3 +17,20 @@ app.use(express.static('public'))
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
+
+// const bcrypt = require('bcryptjs')
+
+// const myFunction = async () => {
+
+//     const password = 'Red1234!'
+//     const hashedPassword = await bcrypt.hash(password, 8)
+
+//     console.log(password)
+//     console.log(hashedPassword)
+
+//     const isMatch = await bcrypt.compare(password, hashedPassword)
+//     console.log(isMatch)
+    
+// }
+
+// myFunction()
--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -30,6 +31,12 @@ const userSchema = new mongoose.Schema({
             }
         }
     }
+})
+
+userSchema.pre('save', async function (next) {
+    const user = this
+    user.password = await bcrypt.hash(user.password, 8)
+    next()
 })
 
 const User = mongoose.model('User', userSchema)
