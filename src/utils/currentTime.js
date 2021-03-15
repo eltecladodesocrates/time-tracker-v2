083f3aa5ca27a5180e238d6fad238a2eb144e8db
Athos Conde de la Fere <eltecladodesocrates@gmail.com>
2021-03-15 06:48:32 -0400

    momentary fix of timezone bug

--- src/utils/currentTime.js
+++ src/utils/currentTime.js
@@ -4,16 +4,16 @@ const currentTime = () => {
 
     const currentHourMinute = moment().format('LT')
     const hourMinuteList = currentHourMinute.split(' ')
-    let hour = parseInt(hourMinuteList[0].split(':')[0])
+    let hour = parseInt(hourMinuteList[0].split(':')[0]) -4
     const minute = hourMinuteList[0].split(':')[1]
-
+    
     if(hourMinuteList[1] === 'AM' && hour == 12) {
         hour = hour - 12
         return hour.toString() + ':' + minute
     }
 
     if(hourMinuteList[1] === 'AM' && hour !== 12) {
-        return hourMinuteList[0]
+        return hour.toString() + ':' + minute
     } else {
         hour = hour + 12
         return hour.toString() + ':' + minute
