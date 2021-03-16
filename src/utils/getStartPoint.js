8f0af911a72b2e303293917f9e4e49ac0bdad838
Athos Conde de la Fere <eltecladodesocrates@gmail.com>
2021-03-16 19:41:44 -0400

    start point fixed

--- src/utils/getStartPoint.js
+++ src/utils/getStartPoint.js
@@ -1,18 +1,27 @@
 const SubTask = require('../models/subTask')
+const moment = require('moment')
 
 const getStartPoint = async (subTaskId) => {
 
     const subTask = await SubTask.findById(subTaskId)
-    const totalSec = subTask.timeStart
-    const startHour = parseInt(totalSec/3600)
-    const startMin = parseInt((totalSec - (3600 * startHour))/60)
+    const time = moment().format('LT').split(' ')[0]
+    const hrs = parseInt(time.split(':')[0]) - 4
+    const min = parseInt(time.split(':')[1])
+
+    console.log(hrs)
+    console.log(min)
+    // const totalSec = subTask.timeStart
+    // const startHour = parseInt(totalSec/3600)
+    // const startMin = parseInt((totalSec - (3600 * startHour))/60)
 
 
-
-
+    // console.log(totalSec)
+    // console.log(startHour)
+    // console.log(startMin)
+    
     // const startHour = parseInt(subTask.timeStart.split(':')[0])
     // const startMin = parseInt(subTask.timeStart.split(':')[1])
-    const startPoint = (startHour * 40) + ((startMin * 40) / 60)
+    const startPoint = (hrs * 40) + ((min * 40) / 60)
     subTask.startPoint = startPoint
     await subTask.save()
 
