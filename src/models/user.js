c6a443c70f054049f7315f2efa0b8166b556d95e
Athos Conde de la Fere <eltecladodesocrates@gmail.com>
2021-03-17 22:40:05 -0400

    no jwt

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
 
@@ -32,23 +31,8 @@ const userSchema = new mongoose.Schema({
                 throw new Error('Invalid email')
             }
         }
-    },
-    tokens: [{
-        token: {
-            type: String,
-            required: true
-        }
-    }]
+    }
 })
-
-userSchema.methods.generateAuthToken = async function () {
-
-    const user = this
-    const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewapp')
-    user.tokens = user.tokens.concat({ token })
-    await user.save()
-    return token
-}
 
 userSchema.statics.findByCredentials = async (email, password) => {
 
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,11 +2,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const User = require('../models/user')
 const router = new express.Router()
-const auth = require('../middleware/auth')
 
 router.use(bodyParser.urlencoded({ extended: true }))
 
-router.get('/users', auth, async (req, res) => {
+router.get('/users', async (req, res) => {
     try {
         const users = await User.find()
         res.send(users)
@@ -23,8 +22,7 @@ router.post('/login', async (req, res) => {
 
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
-        const token = await user.generateAuthToken()
-        res.send({ user, token })
+        res.send({ user })
     } catch (e) {   
         res.status(400).send()
     }
@@ -42,10 +40,9 @@ router.post('/signup', async (req, res) => {
         password: req.body.userPassword,
         email: req.body.userEmail 
     })
-    const token = await user.generateAuthToken()
     await user.save()
     // res.redirect('/')
-    res.send({ user, token })
+    res.send({ user })
 })
 
 module.exports = router
