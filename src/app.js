9cd9b33fb13f19ddbd220105199f060f5ea89d73
Athos Conde de la Fere <eltecladodesocrates@gmail.com>
2021-03-19 08:30:57 -0400

    conncting to db atlas

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const subTaskRouter = require('./routes/subTask')
 require('./db/mongoose')
 
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
 
@@ -22,48 +22,6 @@ app.use(subTaskRouter)
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
-
-
-const substracTime = () => {
-    const timeStart = '13:30'
-    const timeEnd = '15:10'
-
-    const hrsStart = parseInt(timeStart.split(':')[0])
-    const minStart = parseInt(timeStart.split(':')[1])
-
-    const hrsEnd = parseInt(timeEnd.split(':')[0])
-    let minEnd = parseInt(timeEnd.split(':')[1])
-
-    const hrsDifference = hrsEnd - hrsStart
-    if (hrsDifference >= 1) {
-        minEnd = minEnd + (hrsDifference * 60)
-    }
-
-    const passedMin = minEnd - minStart
-    const hrs = parseInt(passedMin/60)
-    const min = passedMin - (hrs * 60)
-
-    return [hrs, min]
-}
-// const moment = require('moment')
-
-// const getStartPoint =  () => {
-
-//     // const subTask = await SubTask.findById(subTaskId)
-//     const time = moment().format('LT').split(' ')[0]
-//     const hrs = parseInt(time.split(':')[0]) - 4
-//     const min = parseInt(time.split(':')[1])
-
-//     const startPoint = (hrs * 40) + ((min * 40) / 60)
-//     // subTask.startPoint = startPoint
-//     // await subTask.save()
-//     console.log(startPoint)
-//     return startPoint
-    
-// }
-
-// getStartPoint()
-
 
 
 
