eb80e365d28e8e1f0a1f1085419879e9e0c993f6
Athos Conde de la Fere <eltecladodesocrates@gmail.com>
2021-03-22 06:23:17 -0400

    utc hrs for heroku

--- src/utils/getRealTime.js
+++ src/utils/getRealTime.js
@@ -1,20 +1,9 @@
 const getRealTime = () => {
 
     const date = new Date()
-    let utcHrs = date.getUTCHours()
+    const utcHrs = (date.getUTCHours()).toString()
     const utcMin = (date.getUTCMinutes()).toString()
-    const offsetTime = date.getTimezoneOffset() / 60
-    if (utcHrs === 0) {
-        utcHrs = 12
-    } else if (utcHrs === 1) {
-        utcHrs = 13
-    } else if (utcHrs === 2) {
-        utcHrs = 14
-    } else if (utcHrs === 3) {
-        utcHrs = 15
-    }
-    const hrs = (utcHrs - offsetTime).toString()
-    const formatedHrs = hrs.length === 1 ? '0' + hrs : hrs
+    const formatedHrs = utcHrs.length === 1 ? '0' + utcHrs : utcHrs
     const formatedMin = utcMin.length === 1 ? '0' + utcMin : utcMin
     const startPoint = (formatedHrs * 40) + ((formatedMin * 40) / 60)
     return {
